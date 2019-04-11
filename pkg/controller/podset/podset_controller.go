ffce52a40444babd21082433bf65b46c7581d113
Robert Rong <robert.rong@agile-solutions.ch>
2019-04-11 14:52:47 +0200

    fix imports

--- pkg/controller/podset/podset_controller.go
+++ pkg/controller/podset/podset_controller.go
@@ -11,7 +11,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     "k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
